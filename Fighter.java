7b439e7642dda988765aafc56e97174b031bc9d0
Aldprll <133879898+Aldprll@users.noreply.github.com>
2023-05-17 22:02:02 +0700

    Create Fighter.java

---
+++ Fighter.java
@@ -0,0 +1,16 @@
+package Character;
+
+public class Fighter extends Hero{
+    public void move() {
+        System.out.println("Fighter is moving");
+    }
+    public void attack() {
+        System.out.println("Fighter is attacking");
+    }
+    public void useSpecialSkills() {
+        System.out.println("Fighter is using special skills");
+    }
+    public void showCombo() {
+        System.out.println("Fighter is showing a combo");
+    }
+}
