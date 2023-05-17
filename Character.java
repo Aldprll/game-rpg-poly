e8cc470cb8245a5c5dfa0a13d6e39de1848c2d52
Aldprll <133879898+Aldprll@users.noreply.github.com>
2023-05-17 22:01:03 +0700

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,10 @@
+package Character;
+
+public class Character {
+    public void move() {
+        System.out.println("Character is moving");
+    }
+    public void attack() {
+        System.out.println("Character is attacking");
+    }
+}
